6c089f1d2efd2e3c31fc3ef647c1481c2005ac58
Nancy <1033580067@qq.com>
2023-08-16 11:50:30 +0800

    添加Icon.vue

---
+++ src/components/Icon.vue
@@ -0,0 +1,28 @@
+<template>
+    <svg class="icon">
+        <use v-bind:xlink:href="'#'+name" />
+    </svg>
+</template>
+
+<script lang="ts">
+    let importAll=(requierContext:__WebpackModuleApi.RequireContext)=>requierContext.keys().forEach(requierContext);
+    try{
+      importAll(require.context('../assets/icons',true,/\.svg$/));
+    }catch(error){
+      console.log(error);
+    }
+    export default {
+        props:['name']
+    }
+</script>
+
+<style lang="scss" scoped>
+    .icon{
+        width: 1em;
+        height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,22 @@
 <template>
     <div class="nav">
       <router-link to="money">
-        <svg>
-            <use xlink-href="#labels" />
-        </svg>
+        <Icon name="money" />
         记账</router-link>
       |
-      <router-link to="labels">标签</router-link>
+      <router-link to="labels">
+        <Icon name="labels" />
+        标签</router-link>
       |
-      <router-link to="statistics">统计</router-link>
+      <router-link to="statistics">
+        <Icon name="statistics" />
+        统计</router-link>
     </div>
 </template>
 
 <script lang="ts">
-    import x from '@/assets/icons/labels.svg'
-    console.log(x)
+    // import x from '@/assets/icons/labels.svg'
+    // console.log(x)
     export default {
         
     }

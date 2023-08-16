cdd881ca88cb45503a9253ceaf1456de96963e29
Nancy <1033580067@qq.com>
2023-08-16 22:06:29 +0800

    更新icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,12 @@
 <template>
     <nav>
-      <router-link to="money" class="item">
+      <router-link to="/money" class="item" active-class="selected">
         <Icon name="money" />
         记账</router-link>
-      <router-link to="labels" class="item">
-        <Icon name="labels" />
+      <router-link to="/labels" class="item" active-class="selected">
+        <Icon name="label" />
         标签</router-link>
-      <router-link to="statistics" class="item">
+      <router-link to="/statistics" class="item" active-class="selected">
         <Icon name="statistics" />
         统计</router-link>
     </nav>
@@ -38,6 +38,8 @@
           height: 2.3em;
         }
       }
-     
+      >.item.selected{
+        color: red;
+      }
     }
 </style>

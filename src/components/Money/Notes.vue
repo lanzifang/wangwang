83544902c7cfdc87069b2a2d00717ccf9744bfa0
Nancy <1033580067@qq.com>
2023-08-22 11:30:16 +0800

    模块化：将money分为四个子组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <label class="notes">
+            <span class="name">备注</span>
+            <input type="text" placeholder="请输入备注...">
+        </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Notes'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .notes{
+        background: #f6f6f6;
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+        padding-left:16px;
+        .name{
+            padding-right: 16px;
+        }
+        input{
+            height: 64px;
+            flex-grow: 1;
+            background: transparent;
+            border: none;
+            padding-right: 16px;
+        }
+    }
+</style>
---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="tags">
+        <div class="new">
+            <button>新增标签</button>
+        </div>
+        <ul class="current">
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Tags'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .tags{
+        font-size: 14px;
+        padding: 16px;
+        display: flex;
+        flex-direction: column-reverse;
+        flex-grow: 1;
+        > .current{
+            display: flex;
+            > li{
+                background: #fda829;
+                $h:24px;
+                height: $h;
+                line-height: $h;
+                border-radius: $h/2;
+                padding: 0 16px;
+                margin-right: 12px;
+            }
+        }
+        > .new{
+            padding-top: 16px;
+            button{
+                background: transparent;
+                border: none;
+                border-bottom: 1px solid;
+                color: #ffd552;
+                padding: 0 3px;
+            }
+        }
+    }
+</style>

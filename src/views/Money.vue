d740395923827b57c3da4c61a9f513131e96b7f4
Nancy <1033580067@qq.com>
2023-09-04 15:59:10 +0800

    将create封装到recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -44,9 +44,7 @@
             this.record.amount=parseFloat(value)
         }
         saveRecord(){
-            const record2:RecordItem=recordListModel.clone(this.record)
-            record2.createdAt=new Date()
-            this.recordList.push(record2)
+            recordListModel.create(this.record)
         }
         @Watch('recordList')
         onRecordListChange(){

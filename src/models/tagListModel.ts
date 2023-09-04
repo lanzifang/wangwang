07873ada9a95a3dd8c13807020d03708cfb8a204
Nancy <1033580067@qq.com>
2023-09-04 13:11:21 +0800

    可以编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -7,8 +7,10 @@ type Tag={
 type TagListModel={
     data:Tag[]
     fetch:()=>Tag[]
-    create:(name:string)=>'success'|'duplicated'
+    create:(name:string)=>'success'|'duplicated'  //联合类型
+    update:(id:string,name:string)=>'success'|'not found'|'duplicated'
     save:()=>void
+    remove:(id:string)=>boolean
 }
 const tagListModel:TagListModel={
     data:[],
@@ -25,8 +27,39 @@ const tagListModel:TagListModel={
         this.save()
         return 'success'
     },
+    update(id,name){
+        const idList=this.data.map(item=>item.id)
+        if(idList.indexOf(id)>=0){
+            const names=this.data.map(item=>item.name)
+            if(names.indexOf(name)>=0){
+                return 'duplicated'
+            }else{
+                const tag=this.data.filter(item=>item.id===id)[0]
+                tag.id=tag.name=name
+                this.save()
+                return 'success'
+            }
+        }else{
+            return 'not found'
+        }
+    },
     save(){
         window.localStorage.setItem(localStorageKeyName,JSON.stringify(this.data))
+    },
+    remove(id:string){
+        let index=-1
+        for(let i=0;i<this.data.length;i++){
+            if(this.data[i].id===id){
+                index=i
+                break
+            }    
+        }
+        console.log('index')
+        console.log(index)
+        this.data.splice(index,1)
+        this.save()
+        console.log(this.data)
+        return true
     }
 }
 export default tagListModel

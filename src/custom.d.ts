13ebc13adec390c336ab560d604cca32ba687876
Nancy <1033580067@qq.com>
2023-09-05 11:46:00 +0800

    封装record的操作

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -25,6 +25,8 @@ declare global{
         removeTag:(id:string)=>boolean
         updateTag:(id:string,name:string)=>'success'|'not found'|'duplicated'
         findTag:(id:string)=>Tag|undefined
+        recordList:RecordItem[]
+        createRecord:(record:RecordItem)=>void
     }
 }
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -13,22 +13,20 @@
 
 <script lang="ts">
     import Vue from 'vue'
-    import {Component,Watch} from 'vue-property-decorator'
+    import {Component} from 'vue-property-decorator'
     import NumberPad from '@/components/Money/NumberPad.vue'
     import Types from '@/components/Money/Types.vue'
     import FormItem from '@/components/Money/FormItem.vue'
     import Tags from '@/components/Money/Tags.vue'
-    import recordListModel from '@/models/recordListModel'
     import RecordItem from '@/custom'
 
-    const recordList=recordListModel.fetch()
 
     @Component({
         components:{NumberPad,Types,FormItem,Tags}
     })
     export default class Money extends Vue {
         tags=window.tagList
-        recordList:RecordItem[]=recordList
+        recordList=window.recordList
         record:RecordItem={
             tags:[],notes:'',type:'-',amount:0
         }
@@ -42,11 +40,7 @@
             this.record.amount=parseFloat(value)
         }
         saveRecord(){
-            recordListModel.create(this.record)
-        }
-        @Watch('recordList')
-        onRecordListChange(){
-            recordListModel.save()
+            window.createRecord(this.record)
         }
     }
 </script>

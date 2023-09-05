f0b3b0de30bbe70f51ae6e0e50be9a4560bd25cc
Nancy <1033580067@qq.com>
2023-09-05 12:50:52 +0800

    将所有数据操作封装在store里

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,14 +19,15 @@
     import FormItem from '@/components/Money/FormItem.vue'
     import Tags from '@/components/Money/Tags.vue'
     import RecordItem from '@/custom'
+    import store from '@/store/index2'
 
 
     @Component({
         components:{NumberPad,Types,FormItem,Tags}
     })
     export default class Money extends Vue {
-        tags=window.tagList
-        recordList=window.recordList
+        tags=store.tagList
+        recordList=store.recordList
         record:RecordItem={
             tags:[],notes:'',type:'-',amount:0
         }
@@ -40,7 +41,7 @@
             this.record.amount=parseFloat(value)
         }
         saveRecord(){
-            window.createRecord(this.record)
+            store.createRecord(this.record)
         }
     }
 </script>
